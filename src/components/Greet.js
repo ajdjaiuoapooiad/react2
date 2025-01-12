f3f337f1f49c625230604567701b4d7436ba3460
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-13 08:17:23 +0900

    greet ok

---
+++ src/components/Greet.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Greeting = () => {
+  return (
+    <div>
+      <div>
+        <h1>Hello people</h1>
+        <ul>
+          <li>
+            <a href='#'>hello world</a>
+            </li>
+        </ul>
+      </div>
+      <h2>hello world</h2>
+    </div>
+  )
+}
+
+export default Greeting
--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Greeting from './components/Greet';
 
 
 
-const Greeting = () => {
-  return (
-    <div>
-      <div>
-        <h1>Hello people</h1>
-        <ul>
-          <li>
-            <a href='#'>hello world</a>
-            </li>
-        </ul>
-      </div>
-      <h2>hello world</h2>
-    </div>
-  )
-}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Greeting />);
