773f2f36fab08a9e0f26690d1c813ff5bb67dea0
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-12 18:00:00 +0900

    first

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+
+const Greeting = () => {
+  return <h2>My first component</h2>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Greeting />);
+
