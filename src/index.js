018896c65f31f1da7a39872975fc8bcb00dff308
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-13 11:08:00 +0900

    map okk

--- src/index.js
+++ src/index.js
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
+const books = [{
+    author: '掌田津耶乃',
+    title: 'React.js＆Next.js超入門 第2版 Kindle版',
+    image: 'https://m.media-amazon.com/images/I/71KXnI8VPqL._SL1500_.jpg',
+
+},{
+    author: '掌田津耶乃',
+    title: 'Ruby on Rails 6 超入門 Kindle版',
+    image: 'https://m.media-amazon.com/images/I/71eRnFOgCEL._SL1500_.jpg',
+},
+]
+
+const names = ['john','pater','yukito']
+const newNames = names.map((name) => {
+    return <h1>{name}</h1>
+})
+console.log(newNames);
 
 
 const BookList = () => {
     return (
         <section className='booklist'>
-            <Book author={book1.author} title={book1.title} image={book1.image} />
-            <Book author={book2.author} title={book2.title} image={book2.image} />
-           
+            {names.map((name) => {
+                return <h1>{name}</h1>
+            })}
         </section>
     )
 }
